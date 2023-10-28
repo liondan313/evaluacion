02eb90f83ee8bd13a25e40ee7b6a6c075562293b
Juan Daniel Lazalde Centeno <liondan313@gmail.com>
2023-10-28 10:44:30 -0700

    inicio

---
+++ backend/tema/src/main/java/com/evaluacion/lazalde/tema/TemaApplication.java
@@ -0,0 +1,16 @@
+package com.evaluacion.lazalde.tema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TemaApplication.class, args);
+		System.out.println("Funcionando");
+
+	}
+
+}
